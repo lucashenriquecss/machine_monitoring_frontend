b6acfb3eab9513032b47a149b1afd472e74096cc
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2025-01-25 13:07:27 -0300

    criação de pages
    maquinas,solicitações, operadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,39 +4,59 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './pages/details/profile/profile.component';
 import { SignComponent } from './pages/sign/sign.component';
 import { MachineComponent } from './pages/details/machine/machine.component';
+import { MachinesComponent } from './pages/machines/machines.component';
+import { SolicitationsComponent } from './pages/solicitations/solicitations.component';
+import { OperatorsComponent } from './pages/operators/operators.component';
 
 export const routes: Routes = [
-  
+
     {
         path: '',
-        redirectTo:'sign',
+        redirectTo: 'sign',
         pathMatch: 'full'
     },
     {
-        path:'',
+        path: '',
         component: SignComponent
-    }, 
+    },
     {
-        path:'',
+        path: '',
         component: LayoutComponent,
-        children:[
+        children: [
             {
-                path:'home',
+                path: 'home',
                 component: HomeComponent,
                 title: 'Home'
             },
             {
-                path:'profile',
+                path: 'profile',
                 component: ProfileComponent,
                 title: 'Profile'
             },
             {
-                path:'machine',
+                path: 'machine',
                 component: MachineComponent,
                 title: 'Machine'
+            },
+            {
+                path: 'machines',
+                component: MachinesComponent,
+                title: 'Machines'
+            },
+
+            {
+                path: 'solicitations',
+                component: SolicitationsComponent,
+                title: 'Solicitations'
+            },
+
+            {
+                path: 'operators',
+                component: OperatorsComponent,
+                title: 'Operators'
             }
         ],
-    }, 
-  
+    },
+
     { path: '**', redirectTo: '/sign' }
 ];
---
+++ src/app/pages/solicitations/solicitations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitations',
+  imports: [],
+  templateUrl: './solicitations.component.html',
+  styleUrl: './solicitations.component.css'
+})
+export class SolicitationsComponent {
+
+}
